0f049361b88630df6e501bb6100f0b8355024da7
Nic Ferier <nic@ferrier.me.uk>
2013-06-12 11:20:16 +0100

    bump the version for the new functions.

--- kv.el
+++ kv.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: lisp
-;; Version: 0.0.15
+;; Version: 0.0.16
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Created: 7th September 2012
 
